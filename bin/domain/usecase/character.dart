6ebc5ecbddba693281386b70ee1bbac3541d8fd5
Meezzi <alswl-3016@naver.com>
2025-03-18 20:48:28 +0900

    feat: Character 클래스 생성

---
+++ bin/domain/usecase/character.dart
@@ -0,0 +1,16 @@
+import 'monster.dart';
+
+class Character {
+  String name;
+  int health;
+  int attack;
+  int defense;
+
+  Character(this.name, this.health, this.attack, this.defense);
+
+  void attackMonster(Monster monster) {}
+
+  void defend() {}
+
+  void showStatus() {}
+}
