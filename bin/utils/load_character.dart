a99f251e1a7dffca65f6ce09cb0150ab2c4da547
Meezzi <alswl-3016@naver.com>
2025-03-19 17:35:01 +0900

    feat: 파일 읽기 오류 처리

--- bin/utils/load_character.dart
+++ bin/utils/load_character.dart
@@ -1,6 +1,12 @@
 import 'dart:io';
 
 Future<String> loadCharacterStatsAsync(String name) async {
-  final file = File('resource/characters.txt');
-  return await file.readAsString();
+  try {
+    final file = File('resource/characters.txt');
+    return await file.readAsString();
+  } on PathNotFoundException {
+    throw ('파일의 지정된 경로를 찾을 수 없습니다.');
+  } catch (e) {
+    throw ('캐릭터 데이터를 불러오는 데 실패했습니다: $e');
+  }
 }
