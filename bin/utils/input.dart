7a2ae8c3fb1d7a7bf4bc2e279b773432b6cd55fb
Meezzi <alswl-3016@naver.com>
2025-03-19 15:06:20 +0900

    feat: 정규 표현식과 일치하지 않으면 안내 메세지 출력 후, 다시 입력

--- bin/utils/input.dart
+++ bin/utils/input.dart
@@ -7,11 +7,14 @@ String? getUserInput() {
 String getUserName() {
   RegExp regexp = RegExp(r'^[a-zA-Z가-힣\s]+$');
 
-  stdout.write('캐릭터의 이름을 입력하세요: ');
-  String? name = stdin.readLineSync();
+  while (true) {
+    stdout.write('캐릭터의 이름을 입력하세요: ');
+    String? name = stdin.readLineSync();
 
-  if (name != null && regexp.hasMatch(name)) {
-    return name;
+    if (name != null && regexp.hasMatch(name)) {
+      return name;
+    } else {
+      print('이름에는 특수문자나 숫자를 포함할 수 없습니다.');
+    }
   }
-  return '';
 }
