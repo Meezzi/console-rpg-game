41fb350ddb5868b7ed6304f40d383d8d3573419f
Meezzi <alswl-3016@naver.com>
2025-03-19 17:36:09 +0900

    feat: 파일에서 읽어온 데이터로 Character 객체 생성

--- bin/rpg_game.dart
+++ bin/rpg_game.dart
@@ -1,7 +1,10 @@
 import '../bin/utils/input.dart';
+import '../bin/domain/usecase/character.dart';
 import 'utils/load_character.dart';
 
 void main() async {
   String name = getUserName();
   String stats = await loadCharacterStatsAsync(name);
+
+  Character character = Character.fromPlainText(name, stats);
 }
