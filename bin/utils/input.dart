bd44eb0e545db6e8b0c250a95ee1142d0fc9a6cc
Meezzi <alswl-3016@naver.com>
2025-03-19 15:02:52 +0900

    feat: 캐릭터 이름 입력 받기

--- bin/utils/input.dart
+++ bin/utils/input.dart
@@ -7,5 +7,8 @@ String? getUserInput() {
 String getUserName() {
   RegExp regexp = RegExp(r'^[a-zA-Z가-힣\s]+$');
 
+  stdout.write('캐릭터의 이름을 입력하세요: ');
+  String? name = stdin.readLineSync();
+
   return '';
 }
