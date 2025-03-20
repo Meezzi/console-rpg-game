0338346fed7e27479878769937505562728ea041
Meezzi <alswl-3016@naver.com>
2025-03-20 21:39:38 +0900

    feat: 게임 객체 생성

--- bin/rpg_game.dart
+++ bin/rpg_game.dart
@@ -1,6 +1,7 @@
 import '../bin/utils/input.dart';
 import '../bin/domain/usecase/character.dart';
 import '../bin/domain/usecase/monster.dart';
+import 'domain/usecase/game.dart';
 import 'utils/load_character.dart';
 import 'utils/load_monster.dart';
 
@@ -11,6 +12,9 @@ void main() async {
 
     Character character = Character.fromPlainText(name, stats);
     List<Monster> monster = loadMonsterStatsAsync();
+
+    // 게임 객체 생성
+    Game game = Game(character, monster);
   } catch (e) {
     print('오류가 발생했습니다 : $e');
   }
