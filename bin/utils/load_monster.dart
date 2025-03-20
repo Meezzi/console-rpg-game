6d0af03a8e8cd0e5c1946c10fc88700b688f1ad3
Meezzi <alswl-3016@naver.com>
2025-03-20 10:54:03 +0900

    feat: 오류 처리

--- bin/utils/load_monster.dart
+++ bin/utils/load_monster.dart
@@ -2,12 +2,19 @@ import 'dart:io';
 import '../domain/usecase/monster.dart';
 
 List<Monster> loadMonsterStatsAsync() {
-  final file = File('resource/monsters.txt');
-  var lines = file.readAsLinesSync();
+  try {
+    final file = File('resource/monsters.txt');
+    var lines = file.readAsLinesSync();
 
-  List<Monster> monsterList = [];
-  for (int i = 0; i < lines.length; i++) {
-    monsterList.add(Monster.fromPlainText(lines[i]));
+    List<Monster> monsterList = [];
+    for (int i = 0; i < lines.length; i++) {
+      monsterList.add(Monster.fromPlainText(lines[i]));
+      print(monsterList[i].attack);
+    }
+    return monsterList;
+  } on PathNotFoundException {
+    throw ('파일의 지정된 경로를 찾을 수 없습니다.');
+  } catch (e) {
+    throw ('몬스터 데이터를 불러오는 데 실패했습니다: $e');
   }
-  return monsterList;
 }
